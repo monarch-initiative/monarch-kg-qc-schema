158194c6ad4ea820db56e1ca153f7f947277e226
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-07-25 10:42:04 -0500

    chore: initial commit

--- src/monarch_kg_qc_schema/datamodel/monarch_kg_qc_schema.py
+++ src/monarch_kg_qc_schema/datamodel/monarch_kg_qc_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from monarch_kg_qc_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-06-20T07:53:30
+# Generation date: 2023-07-25T10:42:02
 # Schema: monarch-kg-qc-schema
 #
 # id: https://w3id.org/monarch-initiative/monarch-kg-qc-schema
@@ -21,8 +21,7 @@ from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
 from rdflib import Namespace, URIRef
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.linkml_model.types import Date, Integer, String, Uriorcurie
-from linkml_runtime.utils.metamodelcore import URIorCURIE, XSDDate
+from linkml_runtime.linkml_model.types import Integer, String
 
 metamodel_version = "1.7.0"
 version = None
@@ -32,8 +31,8 @@ dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
 PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
-BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/')
-EXAMPLE = CurieNamespace('example', 'https://example.org/')
+BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/vocab/')
+EXAMPLE = CurieNamespace('example', 'http://www.example.org/rdf#')
 LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
 MONARCH_KG_QC_SCHEMA = CurieNamespace('monarch_kg_qc_schema', 'https://w3id.org/monarch-initiative/monarch-kg-qc-schema/')
 SCHEMA = CurieNamespace('schema', 'http://schema.org/')
@@ -43,47 +42,11 @@ DEFAULT_ = MONARCH_KG_QC_SCHEMA
 # Types
 
 # Class references
-class NamedThingId(URIorCURIE):
-    pass
 
-
-class ReportId(NamedThingId):
-    pass
 
 
 @dataclass
-class NamedThing(YAMLRoot):
-    """
-    A generic grouping for any identifiable entity
-    """
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = SCHEMA.Thing
-    class_class_curie: ClassVar[str] = "schema:Thing"
-    class_name: ClassVar[str] = "NamedThing"
-    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.NamedThing
-
-    id: Union[str, NamedThingId] = None
-    name: Optional[str] = None
-    description: Optional[str] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, NamedThingId):
-            self.id = NamedThingId(self.id)
-
-        if self.name is not None and not isinstance(self.name, str):
-            self.name = str(self.name)
-
-        if self.description is not None and not isinstance(self.description, str):
-            self.description = str(self.description)
-
-        super().__post_init__(**kwargs)
-
-
-@dataclass
-class Report(NamedThing):
+class Report(YAMLRoot):
     """
     Represents a Report
     """
@@ -94,100 +57,245 @@ class Report(NamedThing):
     class_name: ClassVar[str] = "Report"
     class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.Report
 
-    id: Union[str, ReportId] = None
-    primary_email: Optional[str] = None
-    birth_date: Optional[Union[str, XSDDate]] = None
-    age_in_years: Optional[int] = None
-    vital_status: Optional[Union[str, "PersonStatus"]] = None
+    dangling_edges: Optional[Union[Union[dict, "EdgeReport"], List[Union[dict, "EdgeReport"]]]] = empty_list()
+    edges: Optional[Union[Union[dict, "EdgeReport"], List[Union[dict, "EdgeReport"]]]] = empty_list()
+    nodes: Optional[Union[Union[dict, "NodeReport"], List[Union[dict, "NodeReport"]]]] = empty_list()
+    missing_nodes: Optional[Union[Union[dict, "NodeReport"], List[Union[dict, "NodeReport"]]]] = empty_list()
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, ReportId):
-            self.id = ReportId(self.id)
+        if not isinstance(self.dangling_edges, list):
+            self.dangling_edges = [self.dangling_edges] if self.dangling_edges is not None else []
+        self.dangling_edges = [v if isinstance(v, EdgeReport) else EdgeReport(**as_dict(v)) for v in self.dangling_edges]
 
-        if self.primary_email is not None and not isinstance(self.primary_email, str):
-            self.primary_email = str(self.primary_email)
+        if not isinstance(self.edges, list):
+            self.edges = [self.edges] if self.edges is not None else []
+        self.edges = [v if isinstance(v, EdgeReport) else EdgeReport(**as_dict(v)) for v in self.edges]
 
-        if self.birth_date is not None and not isinstance(self.birth_date, XSDDate):
-            self.birth_date = XSDDate(self.birth_date)
+        if not isinstance(self.nodes, list):
+            self.nodes = [self.nodes] if self.nodes is not None else []
+        self.nodes = [v if isinstance(v, NodeReport) else NodeReport(**as_dict(v)) for v in self.nodes]
 
-        if self.age_in_years is not None and not isinstance(self.age_in_years, int):
-            self.age_in_years = int(self.age_in_years)
-
-        if self.vital_status is not None and not isinstance(self.vital_status, PersonStatus):
-            self.vital_status = PersonStatus(self.vital_status)
+        if not isinstance(self.missing_nodes, list):
+            self.missing_nodes = [self.missing_nodes] if self.missing_nodes is not None else []
+        self.missing_nodes = [v if isinstance(v, NodeReport) else NodeReport(**as_dict(v)) for v in self.missing_nodes]
 
         super().__post_init__(**kwargs)
 
 
 @dataclass
-class ReportCollection(YAMLRoot):
+class SubReport(YAMLRoot):
     """
-    A holder for Report objects
+    Represents a report section for a knowledge graph of entity type
     """
     _inherited_slots: ClassVar[List[str]] = []
 
-    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.ReportCollection
-    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:ReportCollection"
-    class_name: ClassVar[str] = "ReportCollection"
-    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.ReportCollection
+    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.SubReport
+    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:SubReport"
+    class_name: ClassVar[str] = "SubReport"
+    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.SubReport
 
-    entries: Optional[Union[Dict[Union[str, ReportId], Union[dict, Report]], List[Union[dict, Report]]]] = empty_dict()
+    categories: Optional[Union[str, List[str]]] = empty_list()
+    description: Optional[str] = None
+    name: Optional[str] = None
+    namespaces: Optional[Union[str, List[str]]] = empty_list()
+    total_number: Optional[int] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        self._normalize_inlined_as_dict(slot_name="entries", slot_type=Report, key_name="id", keyed=True)
+        if not isinstance(self.categories, list):
+            self.categories = [self.categories] if self.categories is not None else []
+        self.categories = [v if isinstance(v, str) else str(v) for v in self.categories]
+
+        if self.description is not None and not isinstance(self.description, str):
+            self.description = str(self.description)
+
+        if self.name is not None and not isinstance(self.name, str):
+            self.name = str(self.name)
+
+        if not isinstance(self.namespaces, list):
+            self.namespaces = [self.namespaces] if self.namespaces is not None else []
+        self.namespaces = [v if isinstance(v, str) else str(v) for v in self.namespaces]
+
+        if self.total_number is not None and not isinstance(self.total_number, int):
+            self.total_number = int(self.total_number)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class EdgeReport(SubReport):
+    """
+    Represents a sub report for a collection of edges
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.EdgeReport
+    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:EdgeReport"
+    class_name: ClassVar[str] = "EdgeReport"
+    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.EdgeReport
+
+    missing: Optional[int] = None
+    node_types: Optional[Union[Union[dict, "NodeTypeReport"], List[Union[dict, "NodeTypeReport"]]]] = empty_list()
+    predicates: Optional[Union[Union[dict, "PredicateReport"], List[Union[dict, "PredicateReport"]]]] = empty_list()
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self.missing is not None and not isinstance(self.missing, int):
+            self.missing = int(self.missing)
+
+        if not isinstance(self.node_types, list):
+            self.node_types = [self.node_types] if self.node_types is not None else []
+        self.node_types = [v if isinstance(v, NodeTypeReport) else NodeTypeReport(**as_dict(v)) for v in self.node_types]
+
+        if not isinstance(self.predicates, list):
+            self.predicates = [self.predicates] if self.predicates is not None else []
+        self.predicates = [v if isinstance(v, PredicateReport) else PredicateReport(**as_dict(v)) for v in self.predicates]
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class NodeReport(SubReport):
+    """
+    Represents a sub report for a collection of nodes
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.NodeReport
+    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:NodeReport"
+    class_name: ClassVar[str] = "NodeReport"
+    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.NodeReport
+
+    taxon: Optional[Union[str, List[str]]] = empty_list()
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if not isinstance(self.taxon, list):
+            self.taxon = [self.taxon] if self.taxon is not None else []
+        self.taxon = [v if isinstance(v, str) else str(v) for v in self.taxon]
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class NodeTypeReport(NodeReport):
+    """
+    Represents a report for a collection of nodes
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.NodeTypeReport
+    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:NodeTypeReport"
+    class_name: ClassVar[str] = "NodeTypeReport"
+    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.NodeTypeReport
+
+    missing: Optional[int] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self.missing is not None and not isinstance(self.missing, int):
+            self.missing = int(self.missing)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class PredicateReport(YAMLRoot):
+    """
+    Represents a report for predicates of a collection of edges
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.PredicateReport
+    class_class_curie: ClassVar[str] = "monarch_kg_qc_schema:PredicateReport"
+    class_name: ClassVar[str] = "PredicateReport"
+    class_model_uri: ClassVar[URIRef] = MONARCH_KG_QC_SCHEMA.PredicateReport
+
+    missing_object_namespaces: Optional[Union[str, List[str]]] = empty_list()
+    missing_objects: Optional[int] = None
+    missing_subject_namespaces: Optional[Union[str, List[str]]] = empty_list()
+    missing_subjects: Optional[int] = None
+    total_number: Optional[int] = None
+    uri: Optional[str] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if not isinstance(self.missing_object_namespaces, list):
+            self.missing_object_namespaces = [self.missing_object_namespaces] if self.missing_object_namespaces is not None else []
+        self.missing_object_namespaces = [v if isinstance(v, str) else str(v) for v in self.missing_object_namespaces]
+
+        if self.missing_objects is not None and not isinstance(self.missing_objects, int):
+            self.missing_objects = int(self.missing_objects)
+
+        if not isinstance(self.missing_subject_namespaces, list):
+            self.missing_subject_namespaces = [self.missing_subject_namespaces] if self.missing_subject_namespaces is not None else []
+        self.missing_subject_namespaces = [v if isinstance(v, str) else str(v) for v in self.missing_subject_namespaces]
+
+        if self.missing_subjects is not None and not isinstance(self.missing_subjects, int):
+            self.missing_subjects = int(self.missing_subjects)
+
+        if self.total_number is not None and not isinstance(self.total_number, int):
+            self.total_number = int(self.total_number)
+
+        if self.uri is not None and not isinstance(self.uri, str):
+            self.uri = str(self.uri)
 
         super().__post_init__(**kwargs)
 
 
 # Enumerations
-class PersonStatus(EnumDefinitionImpl):
 
-    ALIVE = PermissibleValue(
-        text="ALIVE",
-        description="the person is living",
-        meaning=PATO["0001421"])
-    DEAD = PermissibleValue(
-        text="DEAD",
-        description="the person is deceased",
-        meaning=PATO["0001422"])
-    UNKNOWN = PermissibleValue(
-        text="UNKNOWN",
-        description="the vital status is not known")
-
-    _defn = EnumDefinition(
-        name="PersonStatus",
-    )
 
 # Slots
 class slots:
     pass
 
-slots.id = Slot(uri=SCHEMA.identifier, name="id", curie=SCHEMA.curie('identifier'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.id, domain=None, range=URIRef)
-
-slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.name, domain=None, range=Optional[str])
+slots.categories = Slot(uri=SCHEMA.categories, name="categories", curie=SCHEMA.curie('categories'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.categories, domain=None, range=Optional[Union[str, List[str]]],
+                   pattern=re.compile(r'^\S+:\S+'))
 
 slots.description = Slot(uri=SCHEMA.description, name="description", curie=SCHEMA.curie('description'),
                    model_uri=MONARCH_KG_QC_SCHEMA.description, domain=None, range=Optional[str])
 
-slots.primary_email = Slot(uri=SCHEMA.email, name="primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.primary_email, domain=None, range=Optional[str])
+slots.missing = Slot(uri=MONARCH_KG_QC_SCHEMA.missing, name="missing", curie=MONARCH_KG_QC_SCHEMA.curie('missing'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.missing, domain=None, range=Optional[int])
 
-slots.birth_date = Slot(uri=SCHEMA.birthDate, name="birth_date", curie=SCHEMA.curie('birthDate'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.birth_date, domain=None, range=Optional[Union[str, XSDDate]])
+slots.missing_object_namespaces = Slot(uri=SCHEMA.missing_object_namespaces, name="missing_object_namespaces", curie=SCHEMA.curie('missing_object_namespaces'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.missing_object_namespaces, domain=None, range=Optional[Union[str, List[str]]])
 
-slots.age_in_years = Slot(uri=MONARCH_KG_QC_SCHEMA.age_in_years, name="age_in_years", curie=MONARCH_KG_QC_SCHEMA.curie('age_in_years'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.age_in_years, domain=None, range=Optional[int])
+slots.missing_objects = Slot(uri=MONARCH_KG_QC_SCHEMA.missing_objects, name="missing_objects", curie=MONARCH_KG_QC_SCHEMA.curie('missing_objects'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.missing_objects, domain=None, range=Optional[int])
 
-slots.vital_status = Slot(uri=MONARCH_KG_QC_SCHEMA.vital_status, name="vital_status", curie=MONARCH_KG_QC_SCHEMA.curie('vital_status'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.vital_status, domain=None, range=Optional[Union[str, "PersonStatus"]])
+slots.missing_subject_namespaces = Slot(uri=SCHEMA.missing_subject_namespaces, name="missing_subject_namespaces", curie=SCHEMA.curie('missing_subject_namespaces'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.missing_subject_namespaces, domain=None, range=Optional[Union[str, List[str]]])
 
-slots.reportCollection__entries = Slot(uri=MONARCH_KG_QC_SCHEMA.entries, name="reportCollection__entries", curie=MONARCH_KG_QC_SCHEMA.curie('entries'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.reportCollection__entries, domain=None, range=Optional[Union[Dict[Union[str, ReportId], Union[dict, Report]], List[Union[dict, Report]]]])
+slots.missing_subjects = Slot(uri=MONARCH_KG_QC_SCHEMA.missing_subjects, name="missing_subjects", curie=MONARCH_KG_QC_SCHEMA.curie('missing_subjects'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.missing_subjects, domain=None, range=Optional[int])
 
-slots.Report_primary_email = Slot(uri=SCHEMA.email, name="Report_primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=MONARCH_KG_QC_SCHEMA.Report_primary_email, domain=Report, range=Optional[str],
-                   pattern=re.compile(r'^\S+@[\S+\.]+\S+'))
+slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.name, domain=None, range=Optional[str])
+
+slots.namespaces = Slot(uri=SCHEMA.namespaces, name="namespaces", curie=SCHEMA.curie('namespaces'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.namespaces, domain=None, range=Optional[Union[str, List[str]]])
+
+slots.node_types = Slot(uri=SCHEMA.node_types, name="node_types", curie=SCHEMA.curie('node_types'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.node_types, domain=None, range=Optional[Union[Union[dict, NodeTypeReport], List[Union[dict, NodeTypeReport]]]])
+
+slots.predicates = Slot(uri=SCHEMA.predicates, name="predicates", curie=SCHEMA.curie('predicates'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.predicates, domain=None, range=Optional[Union[Union[dict, PredicateReport], List[Union[dict, PredicateReport]]]])
+
+slots.taxon = Slot(uri=SCHEMA.taxon, name="taxon", curie=SCHEMA.curie('taxon'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.taxon, domain=None, range=Optional[Union[str, List[str]]])
+
+slots.total_number = Slot(uri=MONARCH_KG_QC_SCHEMA.total_number, name="total_number", curie=MONARCH_KG_QC_SCHEMA.curie('total_number'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.total_number, domain=None, range=Optional[int])
+
+slots.uri = Slot(uri=SCHEMA.uri, name="uri", curie=SCHEMA.curie('uri'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.uri, domain=None, range=Optional[str])
+
+slots.report__dangling_edges = Slot(uri=MONARCH_KG_QC_SCHEMA.dangling_edges, name="report__dangling_edges", curie=MONARCH_KG_QC_SCHEMA.curie('dangling_edges'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.report__dangling_edges, domain=None, range=Optional[Union[Union[dict, EdgeReport], List[Union[dict, EdgeReport]]]])
+
+slots.report__edges = Slot(uri=MONARCH_KG_QC_SCHEMA.edges, name="report__edges", curie=MONARCH_KG_QC_SCHEMA.curie('edges'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.report__edges, domain=None, range=Optional[Union[Union[dict, EdgeReport], List[Union[dict, EdgeReport]]]])
+
+slots.report__nodes = Slot(uri=MONARCH_KG_QC_SCHEMA.nodes, name="report__nodes", curie=MONARCH_KG_QC_SCHEMA.curie('nodes'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.report__nodes, domain=None, range=Optional[Union[Union[dict, NodeReport], List[Union[dict, NodeReport]]]])
+
+slots.report__missing_nodes = Slot(uri=MONARCH_KG_QC_SCHEMA.missing_nodes, name="report__missing_nodes", curie=MONARCH_KG_QC_SCHEMA.curie('missing_nodes'),
+                   model_uri=MONARCH_KG_QC_SCHEMA.report__missing_nodes, domain=None, range=Optional[Union[Union[dict, NodeReport], List[Union[dict, NodeReport]]]])
